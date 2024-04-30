edece8c43463a971993059d868ef79e910f56383
Mañana <Maana@AULA8-18>
2024-04-30 09:05:33 +0200

    Proyecto creado

---
+++ src/cliente/Correos.java
@@ -0,0 +1,10 @@
+package cliente;
+
+public class Correos {
+
+    public void enviarCorreo(Cliente cliente) {
+        String direccion = cliente.getDireccion();
+        // Lógica para enviar correo a la dirección
+    }
+
+}
